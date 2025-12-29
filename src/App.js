1df74f0d7f4ff6769ef7b2ca6480bbc7d44c4a0b
PrantikPaul-077 <prantikp22@gmail.com>
2025-12-29 23:52:39 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+import Translator from './components/Translator';
+
+function App() {
+  return (
+    <div>
+      <div className="header">Translator App</div>
+
+    <div className='container'>
+      <div className="wrapper"></div>
+      <Translator></Translator>
+    </div>
+    </div>
+  );
+}
+
+export default App;
